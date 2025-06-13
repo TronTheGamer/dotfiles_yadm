90a84f16f6e0c29d1ea6c76a1ecfef842d541421
yaduraj <yadu1raj@gmail.com>
2025-06-13 08:55:31 +0530

    updated configs with untracked files

---
+++ .config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/trouble.nvim",
+  enable = true,
+  opts = {
+    auto_open = false,
+    auto_close = false,
+    auto_preview = true,
+    auto_fold = false,
+    auto_jump = false,
+    auto_quickfix = false,
+    auto_loclist = false,
+    use_diagnostic_signs = true,
+  },
+}
